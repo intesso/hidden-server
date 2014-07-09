90f06dfd0c0108d4de9bce22091532e7234b9e77
andi-neck <andi.neck@intesso.com>
2014-07-09 07:02:56 +0200

    code cleanup/documentation

--- examples/client.js
+++ examples/client.js
@@ -1,3 +1,4 @@
+// import dependencies
 var request = require('superagent');
 var settings = require('../settings.json');
 
@@ -5,13 +6,18 @@ var settings = require('../settings.json');
 var userArgs = process.argv.slice(2);
 var command = userArgs[0];
 
+// get url right
 var url = settings.publicServer + settings.commandUri;
 url = url.replace(':hiddenServerName', settings.hiddenServerName)
 url = url.replace(':command', command);
 console.log('url', url);
 
+/// send the request
 request
   .get(url)
   .end(function(err, res) {
     console.log('result', url, err, res);
   });
+
+/// alternative client command call
+// curl localhost:3000/command/server1/newCommand
